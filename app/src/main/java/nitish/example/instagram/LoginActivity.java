d99c81728ed73530fbc75eaacb1d19e6cba80064
Nitish Kumar <nitishkumargenius@gmail.com>
2020-06-10 17:05:20 +0530

    Bug Fixes in Signup and Login

--- app/src/main/java/nitish/example/instagram/LoginActivity.java
+++ app/src/main/java/nitish/example/instagram/LoginActivity.java
@@ -1,7 +1,10 @@
 package nitish.example.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,6 +33,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         edtLoginUsername = findViewById(R.id.edtLoginUsername);
         edtLoginPassword = findViewById(R.id.edtLoginPassword);
 
+        edtLoginPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                //user has actually pressed the enter key after filling all the credentials
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+
+                    onClick(btnLogin);
+
+                }
+
+                return false;
+            }
+        });
+
         btnLogin = findViewById(R.id.btnLogin);
 
         txtSignUpPage = findViewById(R.id.txtSignUpPage);
@@ -51,30 +69,57 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId()){
 
             case R.id.btnLogin:
-                ParseUser.logInInBackground(edtLoginUsername.getText().toString(),
-                        edtLoginPassword.getText().toString(), new LogInCallback() {
-                            @Override
-                            public void done(ParseUser user, ParseException e) {
 
-                                if(user != null && e == null){
+                if(edtLoginUsername.getText().toString().equals("") ||
+                        edtLoginPassword.getText().toString().equals("")) {
 
-                                    FancyToast.makeText(LoginActivity.this, user.getUsername() +
-                                            " logged in successfully", Toast.LENGTH_SHORT,
-                                            FancyToast.SUCCESS, true).show();
+                    FancyToast.makeText(LoginActivity.this,
+                            " Username, Password is required!!! ",
+                            Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
+
+                }
+                else {
+                    ParseUser.logInInBackground(edtLoginUsername.getText().toString(),
+                            edtLoginPassword.getText().toString(), new LogInCallback() {
+                                @Override
+                                public void done(ParseUser user, ParseException e) {
+
+                                    if (user != null && e == null) {
+
+                                        FancyToast.makeText(LoginActivity.this, user.getUsername() +
+                                                        " logged in successfully", Toast.LENGTH_SHORT,
+                                                FancyToast.SUCCESS, true).show();
+
+                                    }
 
                                 }
+                            });
 
-                            }
-                        });
-
-
+                }
                 break;
             case R.id.txtSignUpPage:
 
-                //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
 
                 break;
+
+        }
+
+    }
+
+    //hiding the keyboard when user type the empty area on the screen
+    public void rootLayoutTappedLogin(View view){
+
+        try{
+
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+        }
+        catch (Exception e){
+
+            e.printStackTrace();
 
         }
 
--- app/src/main/java/nitish/example/instagram/MainActivity.java
+++ app/src/main/java/nitish/example/instagram/MainActivity.java
@@ -3,7 +3,9 @@ package nitish.example.instagram;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -35,6 +37,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         edtSignUpUsername = findViewById(R.id.edtSignUpUsername);
         edtSignUpPassword = findViewById(R.id.edtSignUpPassword);
 
+        edtSignUpPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+
+                //user has actually pressed the enter key after filling all the credentials
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+
+                    onClick(btnSignUp);
+
+                }
+
+                return false;
+            }
+        });
+
         btnSignUp = findViewById(R.id.btnSignUp);
 
         txtLoginPage = findViewById(R.id.txtLoginPage);
@@ -55,34 +72,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.btnSignUp:
 
-                final ParseUser appUser = new ParseUser();
-                appUser.setEmail(edtSignUpEmail.getText().toString());
-                appUser.setUsername(edtSignUpUsername.getText().toString());
-                appUser.setPassword(edtSignUpPassword.getText().toString());
+                if(edtSignUpEmail.getText().toString().equals("") ||
+                        edtSignUpUsername.getText().toString().equals("") ||
+                        edtSignUpPassword.getText().toString().equals("")) {
 
-                final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-                progressDialog.setMessage("Signin up " + edtSignUpUsername.getText().toString());
-                progressDialog.show();
+                    FancyToast.makeText(MainActivity.this,
+                                    " Email, Username, Password is required!!! ",
+                            Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
 
-                appUser.signUpInBackground(new SignUpCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if(e == null){
-                            FancyToast.makeText(MainActivity.this, appUser.getUsername() +
-                                    " is signed up successfully", Toast.LENGTH_SHORT, FancyToast.SUCCESS,
-                                    true).show();
+                }
+                else {
+                    final ParseUser appUser = new ParseUser();
+                    appUser.setEmail(edtSignUpEmail.getText().toString());
+                    appUser.setUsername(edtSignUpUsername.getText().toString());
+                    appUser.setPassword(edtSignUpPassword.getText().toString());
+
+                    final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setMessage("Signin up " + edtSignUpUsername.getText().toString());
+                    progressDialog.show();
+
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                FancyToast.makeText(MainActivity.this, appUser.getUsername() +
+                                                " is signed up successfully", Toast.LENGTH_SHORT, FancyToast.SUCCESS,
+                                        true).show();
+                            } else {
+                                FancyToast.makeText(MainActivity.this,
+                                        " There was an error: " + e.getMessage(),
+                                        Toast.LENGTH_SHORT, FancyToast.ERROR, true).show();
+                            }
+                            progressDialog.dismiss();
                         }
-                        else{
-                            FancyToast.makeText(MainActivity.this,
-                                    " There was an error: " + e.getMessage(),
-                                    Toast.LENGTH_SHORT, FancyToast.ERROR, true).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
+                    });
 
+                }
                 break;
-
             case R.id.txtLoginPage:
 
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
@@ -93,4 +119,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    //hiding the keyboard when user type the empty area on the screen
+    public void rootLayoutTapped(View view){
+
+        try{
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+    }
+
 }
